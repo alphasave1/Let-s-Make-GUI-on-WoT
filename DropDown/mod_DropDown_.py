ae3c2fdaf49a5ed6842cc4a8c7ad1032ba1b1b87
alphasave1 <akimitu9717@gmail.com>
2017-11-05 22:43:37 +0900

    ModsListApi
    Added Item on ModsListApi.

---
+++ DropDown/mod_DropDown_.py
@@ -0,0 +1,39 @@
+from gui.Scaleform.framework import g_entitiesFactories, ViewSettings
+from gui.Scaleform.framework import ViewTypes, ScopeTemplates
+from gui.Scaleform.framework.entities.abstract.AbstractWindowView import AbstractWindowView
+from gui.app_loader import g_appLoader
+from gui.Scaleform.framework.managers.loaders import ViewLoadParams
+from gui.shared.utils.key_mapping import getBigworldNameFromKey
+from gui.modsListApi import g_modsListApi
+class TestWindow(AbstractWindowView):
+
+    def __init__(self):
+        super(TestWindow, self).__init__()
+
+    def _populate(self):
+        super(TestWindow, self)._populate()
+
+    def onWindowClose(self):
+        self.destroy()
+
+_alias = 'Main'
+_url = 'DropDown.swf'
+_type = ViewTypes.WINDOW
+_event = None
+_scope = ScopeTemplates.VIEW_SCOPE
+
+
+_settings = ViewSettings(_alias, TestWindow, _url, _type, _event, _scope)
+g_entitiesFactories.addSettings(_settings)
+
+def onhandleKeyEvent(event):
+    key = getBigworldNameFromKey(event.key)
+    if key == 'KEY_F11':
+        g_appLoader.getDefLobbyApp().loadView(ViewLoadParams(_alias, None))
+    return None
+
+def init() :
+    g_modsListApi.addModification(id='DropDown',name='DropDown',description='DropDownMenu Test Program.',enabled=True,callback=lambda: g_appLoader.getDefLobbyApp().loadView(ViewLoadParams(_alias, None)),login=True,lobby=False,icon='')
+
+from gui import InputHandler
+InputHandler.g_instance.onKeyDown += onhandleKeyEvent
